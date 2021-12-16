07b9e32d63bde2c3e40dddd454f9e82305b1a9ba
Igor Katsuba <igor@katsuba.dev>
2021-12-16 15:34:03 +0300

    feat: creates the `runners` library

---
+++ libs/runners/src/lib/runners.spec.ts
@@ -0,0 +1,7 @@
+import { runners } from './runners';
+
+describe('runners', () => {
+  it('should work', () => {
+    expect(runners()).toEqual('runners');
+  });
+});
---
+++ libs/runners/src/lib/runners.ts
@@ -0,0 +1,3 @@
+export function runners(): string {
+  return 'runners';
+}
